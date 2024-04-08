c023fc4cf308c39e5d22ac67938be8256c21f78a
Sagar Thapa <kingstonboysagar@gmail.com>
2024-04-08 17:24:05 +0545

    quiz project

---
+++ quizgame.py
@@ -0,0 +1,24 @@
+# Quize game
+class Quize:
+    def __init__(self,question,answer) -> None:
+        self.question=question
+        self.answer=answer
+questions=[
+    "1.what is the full form of ABC in python?\n a.Abstract Base class\n b.Apple Base class\n c.Nothing\n d.do not know",
+    "2.what is the full form of OOPS in python?\n a.object oriented python\n b.object oriented programming language\nc.object oral programming",
+    "3.what is the full form of ATM\n a.automatic teller machine\n b.automated telling machine\n c.automated teller macine",
+]
+
+quizList=[Quize(questions[0],'a'),
+          Quize(questions[1],'b'),
+          Quize(questions[2],'c'),]
+
+def show(quizList):
+    for i in quizList:
+        ask=input(f"{i.questions}\n enter your answer :")
+        if ask==i.answer:
+            marks+=1
+        else:
+            print(f"you choose wrong answer,your correct answer is {i.answer} ")
+    print(f"your marks is{marks/{len(questions)}}")
+show(quizList)
